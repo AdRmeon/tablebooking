7d91fdc6d79e66912a914f4d2633a2104b49c538
Vladimir Adrmeon <adrmeon@gmail.com>
2025-04-08 15:41:16 +0300

    init

---
+++ app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "tablebooking"
+
+    @property
+    def database_url(self) -> PostgresDsn:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
